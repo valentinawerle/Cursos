d07223edbee137d90587425cb0e8de3f1293424d
Valentina Werle <147965244+valentinawerle@users.noreply.github.com>
2024-12-17 11:42:48 -0300

    ejercicios

---
+++ workspace/ultimatejs/03-control-flujo/10-switch.js
@@ -0,0 +1,15 @@
+
+let accion = 'listar';
+
+switch (accion){
+    case 'listar':
+        console.log('Accion de listar');
+        break;
+
+    case 'guardar':
+        console.log('Accion de guardar');
+        break;
+    default:
+        console.log('Accion no reconocida');
+}
+
---
+++ workspace/ultimatejs/04-ejercicios/01.js
@@ -0,0 +1,10 @@
+function cualEsMayor(a, b){
+    
+    return (a > b) ? a : b;
+
+}
+    
+
+let mayor = cualEsMayor(10, 5);
+
+console.log(mayor);
