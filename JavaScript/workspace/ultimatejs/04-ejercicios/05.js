522c95db383bafb19c32e1abbe7bf4cc6f5974fa
Valentina Werle <147965244+valentinawerle@users.noreply.github.com>
2024-12-18 13:58:21 -0300

    Update 05.js

--- JavaScript/workspace/ultimatejs/04-ejercicios/05.js
+++ JavaScript/workspace/ultimatejs/04-ejercicios/05.js
@@ -1,25 +1,15 @@
-/**
- * Crear un algoritmo que devuelva el numero menor 
- * y el mayor de un array
- */
+let array = [2, 5, 7, 15, -5, -100, 55];
 
-let array = [2, 7, 5, 15, -5, -100, 55];
-
-function getMenorMayor(arr){
-    menor= arr[0];
-    mayor= arr[0];
-    for(numero of arr){
-
-        menor < numero ? menor : numero;
-        mayor > numero ? mayor : numero;
-
+function getMenorMayor(arr) {
+    let menor = arr[0];
+    let mayor = arr[0];
+    for (let numero of arr) {
+        menor = numero < menor ? numero : menor; // Actualizar `menor` si `numero` es menor
+        mayor = numero > mayor ? numero : mayor; // Actualizar `mayor` si `numero` es mayor
     }
 
     return [menor, mayor];
-
 }
 
-let numeros= getMenorMayor(array)
-console.log(numeros);
-
-
+let numeros = getMenorMayor(array);
+console.log(numeros); // [-100, 55]
