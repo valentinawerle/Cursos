71bc9a720b5ff4db3a0061d7d66758032da24a41
Valentina Werle <147965244+valentinawerle@users.noreply.github.com>
2024-12-23 12:09:46 -0300

    Update settings.py

--- Django/personas-django/sap/sap/settings.py
+++ Django/personas-django/sap/sap/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = "django-insecure-jcw6bi7&&wkblup90-5+#lnb^f_0h_sw7(023u57ig&m1@0s-#
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -71,17 +69,25 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "sap.wsgi.application"
 
-
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+#DATABASES = {
+#    "default": {
+#       "ENGINE": "django.db.backends.sqlite3",
+#      "NAME": BASE_DIR / "db.sqlite3",
+# }
+#}
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "sap_db",
+        "PASSWORD": "hola123",
+        "HOST": "localhost",
+        "PORT": "5000"
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
@@ -101,7 +107,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
@@ -112,7 +117,6 @@ TIME_ZONE = "UTC"
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
