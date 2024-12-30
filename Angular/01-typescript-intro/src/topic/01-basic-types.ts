9abd9a8ceeae6618e6bcdc9c8efa36a36f82b807
Valentina Werle <147965244+valentinawerle@users.noreply.github.com>
2024-12-30 10:47:17 -0300

    curso

---
+++ Angular/01-typescript-intro/src/topic/01-basic-types.ts
@@ -0,0 +1,13 @@
+
+const name: string = 'Valen';
+let hpPoints: number | 'completo' = 95;
+const isAlive: boolean = true;
+
+hpPoints = 'completo'
+
+
+console.log(name, hpPoints, isAlive);
+
+
+
+export{};
---
+++ Angular/01-typescript-intro/src/topic/02-object-interface.ts
@@ -0,0 +1,26 @@
+
+const skills: string[]= ['Bash', 'Counter', 'Healing'];
+
+interface Character {
+    name: string;
+    hp: number;
+    skills: string[];
+    hometown?: string;
+}
+
+const strider: Character = {
+    name: 'Strider',
+    hp: 100,
+    skills: ['Bash', 'Counter'],
+
+};
+
+strider.hometown = 'Buenos Aires';
+
+console.table(strider);
+
+
+
+
+
+export{};
