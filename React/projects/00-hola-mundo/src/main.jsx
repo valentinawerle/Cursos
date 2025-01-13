f08e650a3f3958de46af98603666831a8bdcb941
Valentina Werle <valentinawerle@gmail.com>
2025-01-13 14:10:42 -0300

    react

--- React/projects/00-hola-mundo/src/main.jsx
+++ React/projects/00-hola-mundo/src/main.jsx
@@ -1,7 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-
+import { App } from './TwitterFollowCard.jsx'
+import './index.css'
 
 
 createRoot(document.getElementById('root')).render(
-<button>Hola button</button>)
+
+
+<App/>
+
+
+
+)
