08e41e1f1a5990466246154170adf4afc7495501
Valentina <valentinawerle@gmail.com>
2025-01-17 14:12:11 -0300

    react

---
+++ React/03-counter-app-vite/src/FirstApp.jsx
@@ -0,0 +1,22 @@
+import PropTypes from 'prop-types';
+
+export const FirstApp = ({ title, subTitle }) => {
+
+    //console.log(title);
+
+    
+
+    return( 
+    <> 
+        <h1>{ title }</h1>
+        {/*<code>{JSON.stringify(newMessage)}</code>*/}
+        <p>{ subTitle}</p>
+    </>
+
+    )
+}
+
+FirstApp.propTypes = {
+    title: PropTypes.string.isRequired,
+    subTitle: PropTypes.number.isRequired
+}
---
+++ React/03-counter-app-vite/src/HelloWorldApp.jsx
@@ -0,0 +1,6 @@
+
+export const HelloWorldApp = () => {
+  return (
+    <div>Hello World App</div>
+  )
+}
