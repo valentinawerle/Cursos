cd897ed24598f555ec23315caf10d6e1e4e8877a
Valentina Werle <valentinawerle@gmail.com>
2025-01-24 18:20:50 -0300

    apps angular

--- Angular/02-bases/src/app/dbz/pages/main-page.components.ts
+++ Angular/02-bases/src/app/dbz/pages/main-page.components.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Character } from '../interfaces/character.interface';
+import { DbzService } from '../services/dbz.service';
+
 
 @Component({
     selector: 'app-dbz-main-page',
@@ -6,6 +9,21 @@ import { Component } from '@angular/core';
     templateUrl: './main-page.component.html'
 })
 
-export class MainPageComponent{
-    saludo: string = "hola";
+export class MainPageComponent {
+
+    constructor( private dbzService: DbzService ){}
+
+    get characters(): Character[]{
+        return [...this.dbzService.characters]
+    }
+
+    onDeleteCharacter(id: string): void {
+        this.dbzService.deleteCharacterById(id)
+    }
+
+    onNewCharacter( character: Character): void {
+        this.dbzService.addCharacter(character);
+    }
+
+
 }
