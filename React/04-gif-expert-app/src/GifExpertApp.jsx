65578da14b14fd05bc853bda5865071084b3d4e3
Valentina <valentinawerle@gmail.com>
2025-01-29 11:44:32 -0300

    react

---
+++ React/04-gif-expert-app/src/GifExpertApp.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react"
+import { AddCategory } from "./components/AddCategory";
+
+export const GifExpertApp = () => {
+
+    const [categories, setCategories] = useState(['Hatsune Miku', 'Pokemon']);
+
+    const onAddCategory = () => {
+
+      setCategories([...categories, "Valorant" ]);
+    
+    }
+
+  return (
+
+    <>
+    {/*Titulo*/}
+    <h1>GifsExpertApp</h1>
+
+    {/*Titulo*/}
+    <AddCategory/>
+
+    <button onClick= {onAddCategory}>Agregar</button>
+
+    {/*Listado*/}
+    <ol>
+        {
+            categories.map(category => {
+                return <li key ={ category } > {category} </li>
+            })
+        }
+    </ol>
+    </>
+
+
+  )
+}
